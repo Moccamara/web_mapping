bc876d1fea4608d77f3c4544a32cb1bfda506117
Moccamara <mahamadoucamaramoc@outlook.com>
2025-12-25 15:47:15 +0000

    Update dashdoardmc.py

--- dashdoardmc.py
+++ dashdoardmc.py
@@ -56,8 +56,7 @@ else:
 # =========================================================
 # LOAD SE POLYGONS FROM GITHUB (RAW)
 # =========================================================
-# SE_URL = "https://raw.githubusercontent.com/Moccamara/web_mapping/main/data/SE.geojson"
-SE_URL ="https://github.com/Moccamara/web_mapping/blob/main/data/SE.geojson"
+SE_URL = "https://raw.githubusercontent.com/Moccamara/web_mapping/main/data/SE.geojson"
 @st.cache_data(show_spinner=False)
 def load_se_data(url):
     try:
@@ -245,6 +244,7 @@ st.markdown("""
 **Geospatial Enterprise Web Mapping** Developed with Streamlit, Folium & GeoPandas  
 **Mahamadou CAMARA, PhD – Geomatics Engineering** © 2025
 """)
+
 
 
 
