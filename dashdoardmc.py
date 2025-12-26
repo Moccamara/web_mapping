50d82b2b5385dd6da5308bbefa377079a3e0d75a
Moccamara <mahamadoucamaramoc@outlook.com>
2025-12-26 00:08:30 +0000

    Update dashdoardmc.py

--- dashdoardmc.py
+++ dashdoardmc.py
@@ -38,7 +38,7 @@ def logout():
     st.session_state.username = None
     st.session_state.user_role = None
     st.session_state.page = "Home"
-    st.experimental_rerun()
+    # st.experimental_rerun()
 
 # =========================================================
 # HOME PAGE (Login)
@@ -241,4 +241,5 @@ st.markdown("""
 Developed with Streamlit, Folium & GeoPandas  
 **Mahamadou CAMARA, PhD – Geomatics Engineering** © 2025
 """)
+
 
