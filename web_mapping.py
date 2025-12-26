a088e30b28e82b4e817aebf63847ee490d8b476c
Moccamara <mahamadoucamaramoc@outlook.com>
2025-12-26 18:53:03 +0000

    Update web_mapping.py

--- web_mapping.py
+++ web_mapping.py
@@ -194,7 +194,7 @@ with col_chart:
             alt.Chart(df_long)
             .mark_bar()
             .encode(
-                x=alt.X("idse_new:N", title=None, axis=alt.Axis(labelAngle=90)),
+                x=alt.X("idse_new:N", title=None, axis=alt.Axis(labelAngle=0)),
                 xOffset="Variable:N",
                 y=alt.Y("Population:Q", title=None),
                 color=alt.Color("Variable:N", legend=alt.Legend(orient="right", title="Type")),
@@ -248,5 +248,6 @@ st.markdown("""
 **Geospatial Enterprise Web Mapping** Developed with Streamlit, Folium & GeoPandas  
 **Mahamadou CAMARA, PhD – Geomatics Engineering** © 2025
 """)
+
 
 
